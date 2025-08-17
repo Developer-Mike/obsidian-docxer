8411dfc82b2c35dfbf91a481f3e2adad08acdde0
Developer-Mike <delta.cephei@outlook.com>
2025-08-17 15:55:48 +0200

    Moved output to dist symlink folder

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -2,6 +2,7 @@ import esbuild from "esbuild";
 import process from "process";
 import builtins from "builtin-modules";
 import { sassPlugin } from 'esbuild-sass-plugin';
+import { copy } from "esbuild-plugin-copy";
 
 const banner =
 `/*
@@ -38,9 +39,14 @@ const context = await esbuild.context({
 	logLevel: "info",
 	sourcemap: prod ? false : "inline",
 	treeShaking: true,
-  outdir: ".",
+  outdir: "dist",
 	plugins: [
 		sassPlugin({}),
+    copy({
+      resolveFrom: "cwd",
+      watch: !prod,
+      assets: [ { from: "manifest.json", to: "dist/manifest.json" } ],
+    }),
 	],
 });
 
